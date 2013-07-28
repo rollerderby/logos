0322286a7bfc33b2d68208cde405296e8962df29
Rob Thomas <xrobau@gmail.com>
2013-07-28 13:41:21 +1000

    Will write a basic managing app for this.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<html>
+  <head>
+    <title>Roller Derby League Logo manager</title>
+  </head>
+<?php
+// Iterate through all files, and update/display/create info about each of them.
+// Nothing here yet. Move along.
+
+
+?>
+</html>
+
+
+
